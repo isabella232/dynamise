2fd55b1d4c3be23a2dda3cbdb0293bce2cbec66c
meaku <michael.jaser@peerigon.com>
2015-03-27 11:11:37 +0100

    Update example

--- example/example.js
+++ example/example.js
@@ -17,17 +17,31 @@ db.setSchemas(schemas);
 //db("local").create("user"); //or .create(tables.user) //-> an object with table definition
 
 /*
-db("local").read("user")
-    .then(function (res) {
-        console.log(res);
-    })
-    .catch(function (err) {
-        //TODO throw err gets caught..
-        console.log(err, err.stack);
-    });
-*/
+ db("local").read("user")
+ .then(function (res) {
+ console.log(res);
+ })
+ .catch(function (err) {
+ console.log(err, err.stack);
+ });
+ */
 
+//check if table exists (maybe .exists() method later on?)
 db("local").status("user")
+    .catch(function (err) {
+
+        //create if not exists
+        if (err.code === "ResourceNotFoundException") {
+            return db("local").create("user");
+        }
+
+        //forward error to global handler if a different error occurend
+        throw err;
+    })
+    .then(function () {
+        //fetch the status
+        return db("local").status("user");
+    })
     .then(function (res) {
         console.log("status", res);
     })

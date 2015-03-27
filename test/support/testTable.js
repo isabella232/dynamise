abead3dbb3fb2c8456e77bbc91715f62e055d198
meaku <michael.jaser@peerigon.com>
2015-03-27 12:27:28 +0100

    Add test table

---
+++ test/support/testTable.js
@@ -0,0 +1,45 @@
+"use strict";
+
+module.exports = {
+    TableName: "test",
+    AttributeDefinitions: [
+        { AttributeName: "id", AttributeType: "S" },
+        { AttributeName: "email", AttributeType: "S" },
+        { AttributeName: "password", AttributeType: "S" }
+    ],
+    KeySchema: [
+        { AttributeName: "id", KeyType: "HASH" }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    },
+    GlobalSecondaryIndexes: [
+        {
+            IndexName: "idIndex",
+            KeySchema: [
+                { AttributeName: "id", KeyType: "HASH" }
+            ],
+            Projection: {
+                ProjectionType: "ALL"
+            },
+            ProvisionedThroughput: {
+                ReadCapacityUnits: 10,
+                WriteCapacityUnits: 10
+            }
+        },
+        {
+            IndexName: "emailIndex",
+            KeySchema: [
+                { AttributeName: "email", KeyType: "HASH" }
+            ],
+            Projection: {
+                ProjectionType: "ALL"
+            },
+            ProvisionedThroughput: {
+                ReadCapacityUnits: 10,
+                WriteCapacityUnits: 10
+            }
+        }
+    ]
+};

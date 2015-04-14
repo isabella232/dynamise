84142ab3998b050262ee8ef7366f2cf7eb9caa8c
meaku <michael.jaser@peerigon.com>
2015-04-14 10:31:40 +0200

    Add proper upload implementation and tests

--- test/integration/stream/upload.test.js
+++ test/integration/stream/upload.test.js
@@ -1,6 +1,10 @@
 "use strict";
 
-var expect = require("chai").expect;
+var path = require("path"),
+  fs = require("fs"),
+  expect = require("chai").expect,
+  _ = require("lodash"),
+  JSONStream = require("JSONStream");
 
 var db = require("../../../lib/");
 var testTable = require("../../support/testTable");
@@ -32,60 +36,119 @@ describe("Table", function () {
       .then(function () {
         return client.create("test");
       })
-    .then(function(res) {
+      .then(function (res) {
         return client.active("test");
-      })
-    .then(function(res) {
-        console.log(res);
       });
   });
 
   describe("UploadStream", function () {
 
-    it("should work!", function (done) {
+    describe("as a stream", function () {
 
-      var data = [
-        {
-          id: "1",
-          email: "mj@peerigon.com"
-        },
-        {
-          id: "2",
-          email: "md@peerigon.com"
-        }
-      ];
+      it("should create all items in the database writing to the stream manually", function (done) {
 
-      var upload = client.table("test").upload();
+        var data = [];
 
-      upload.on("finish", done);
-
-      upload.on("error", done);
-
-      write();
-
-      function write() {
-        var ok = true,
-          currentItem;
-
-        while (ok && (currentItem = data.shift()) !== undefined) {
-
-          if (data.length === 0) {
-            upload.end(currentItem);
-            break;
-          }
-
-          ok = upload.write(currentItem, function () {
+        for (var i = 0; i < 123; i++) {
+          data.push({
+            id: i.toString(),
+            email: i + "@epha.com"
           });
+        }
 
-          if (!ok) {
-            upload.once("drain", write);
-            break;
+        var expectedData = _.clone(data, true);
+
+        var upload = client.table("test").upload();
+
+        upload.on("finish", function () {
+
+          Promise.all(expectedData.map(function (element) {
+            return client.table("test").read(element.id, element.email)
+              .then(function (res) {
+                expect(res).not.eql(undefined);
+                expect(expectedData).contains(res);
+              });
+          })).then(function (res) {
+            done();
+          }, done);
+        });
+
+        upload.on("error", done);
+
+        write();
+
+        function write() {
+          var ok = true,
+            currentItem;
+
+          while (ok && (currentItem = data.shift()) !== undefined) {
+
+            if (data.length === 0) {
+              upload.end(currentItem);
+              break;
+            }
+
+            ok = upload.write(currentItem, function () {
+            });
+
+            if (!ok) {
+              upload.once("drain", write);
+              break;
+            }
           }
         }
-      }
+      });
     });
 
+    describe("with promise syntax passing an array", function () {
 
+      it("should create all items passed in the array using a stream internally", function () {
+
+        var data = [];
+
+        for (var i = 0; i < 123; i++) {
+          data.push({
+            id: i.toString(),
+            email: i + "@epha.com"
+          });
+        }
+
+        var expectedData = _.clone(data, true);
+
+        return client.table("test").upload(data)
+          .then(function () {
+            Promise.all(expectedData.map(function (element) {
+              return client.table("test").read(element.id, element.email)
+                .then(function (res) {
+                  expect(res).not.eql(undefined);
+                  expect(expectedData).contains(res);
+                });
+            }));
+          });
+      });
+    });
+
+    describe("with promise syntax passing a readable stream", function () {
+
+      it("should create all items of the readable stream", function () {
+
+        var expectedData = require("../../support/testData.json"),
+          jsonStringStream = fs.createReadStream(path.resolve(__dirname, "../../support/testData.json")),
+          jsonStream = JSONStream.parse();
+
+        jsonStringStream.pipe(jsonStream);
+
+        return client.table("test").upload(jsonStream)
+          .then(function () {
+            Promise.all(expectedData.map(function (element) {
+              return client.table("test").read(element.id, element.email)
+                .then(function (res) {
+                  expect(res).not.eql(undefined);
+                  expect(expectedData).contains(res);
+                });
+            }));
+          });
+      });
+    });
   });
-
 });

3b3bc50d6debdb64b290734a82cc7911a1d13779
Jonas74 <marco.egbring@epha.ch>
2015-04-17 15:44:06 +0200

    need for working upload

--- tables/index.js
+++ tables/index.js
@@ -7,7 +7,6 @@ exports.subject = require("./subject");
 exports.role = require("./role");
 exports.rule = require("./rule");
 exports.policy = require("./policy");
-exports.TestTable = require("./TestTable");
 
 exports.Pubmed = require("./Pubmed");
 exports.Atc = require("./Atc");

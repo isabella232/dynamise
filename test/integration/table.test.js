f5cd08127d75c4036e7503d6f3cfb1ee601c91d2
Markus Dobmann <md@noqq.eu>
2015-05-05 11:04:59 +0200

    Add test for scan()

--- test/integration/table.test.js
+++ test/integration/table.test.js
@@ -185,6 +185,62 @@ describe("Table", function () {
     });
   });
 
+  describe("#scan", function () {
+
+    var items = [];
+    var limit = 5;
+
+    describe("with 53 items in a database", function () {
+
+      beforeEach(function () {
+
+        for (var i = 0; i < 53; i++) {
+          items.push({
+            "id": "" + i,
+            "email": i + "@epha.com"
+          });
+        }
+
+        return client.create(testTable)
+          .then(function () {
+            return client.active(testTable.TableName);
+          })
+          .then(function () {
+            return client.table(testTable.TableName).upload(items);
+          });
+      });
+
+      it("should scan " + limit + " items with '{Limit: " + limit + "}' param and have a LastEvaluatedKey", function () {
+
+        return client.table(testTable.TableName).scan({Limit: limit})
+          .then(function (data) {
+            expect(data).to.be.instanceof(Object);
+            expect(data.Items).to.be.instanceof(Array);
+            expect(data.Items).to.have.length(limit);
+            expect(data).to.have.property("LastEvaluatedKey");
+          });
+      });
+
+      it("should scan 53 items without 'Limit' param", function () {
+
+        return client.table(testTable.TableName).scan()
+          .then(function (data) {
+            expect(data).to.be.instanceof(Object);
+            expect(data.Items).to.be.instanceof(Array);
+            expect(data.Items).to.have.length(53);
+          });
+      });
+
+      afterEach(function () {
+        return client.remove(testTable.TableName)
+          .catch(function () {
+            return true;
+          });
+      });
+    });
+
+  });
+
   describe("#download", function () {
 
     var items = [];
@@ -217,7 +273,10 @@ describe("Table", function () {
     });
 
     afterEach(function () {
-      return client.remove(testTable.TableName);
+      return client.remove(testTable.TableName)
+        .catch(function () {
+          return true;
+        })
     })
 
   });

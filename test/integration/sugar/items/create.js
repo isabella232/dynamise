dc8e5adaf6627cdf41dc173da91625d6f12c8cd5
Markus Dobmann <md@noqq.eu>
2015-05-11 08:33:34 +0200

    Add tests for items functions - part 1

--- test/integration/sugar/items/create.js
+++ test/integration/sugar/items/create.js
@@ -52,6 +52,31 @@ describe("create an item - client.table(tableName).create()", function () {
           return true
         });
     });
+  });
 
+  describe("if the item does not exist", function () {
+
+    beforeEach(function () {
+      return client.create(testTable);
+    });
+
+    it("should write item to the database", function () {
+      return client.table(testTable.TableName).create(newItem)
+        .then(function () {
+          return client.table(testTable.TableName).download();
+        })
+        .then(function (data) {
+          expect(data).to.be.instanceof(Array);
+          expect(data).to.have.length(1);
+          expect(data[0]).to.eql(newItem);
+        });
+    });
+
+    afterEach(function () {
+      return client.remove(testTable)
+        .catch(function () {
+          return true;
+        });
+    });
   });
 });
---
+++ test/integration/sugar/items/multi/upsert.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var expect = require("chai").expect;
+
+var db = require("../../../../../lib");
+var testTable = require("../../../../support/testTable");
+
+describe("multi upsert items - client.table(table).multiUpsert()", function () {
+
+  var client;
+
+  beforeEach(function () {
+
+    client = db("local");
+    client.set({test: testTable});
+
+    return client.remove(testTable)
+      .catch(function (err) {
+        return true;
+      });
+  });
+
+  describe("if the database is empty", function () {
+
+    beforeEach(function () {
+      return client.create(testTable)
+    });
+
+    var items = [];
+    for (var i = 1; i < 188; i++) {
+      items.push({
+        id: "" + i,
+        email: i + "@epha.com"
+      });
+    }
+
+    it("should properly insert all items if database is empty", function () {
+
+      return client.table(testTable.TableName).multiUpsert(items)
+        .then(function () {
+          return client.table(testTable.TableName).download();
+        })
+        .then(function (res) {
+          // test if arrays have the same length
+          expect(res).to.be.instanceof(Array);
+          expect(res).to.have.length(items.length);
+
+          // and now test if the items are equal
+          res.forEach(function (item) {
+            expect(items).to.contain(item);
+          });
+        });
+    });
+  });
+
+  afterEach(function () {
+    return client.remove(testTable)
+      .catch(function () {
+        return true;
+      });
+  });
+});

f99261c70d242b20d500ce52c1577837f5ba482a
Markus Dobmann <md@noqq.eu>
2015-05-19 21:43:36 +0200

    Refactor active.js / active.test.js - Make tests work again

--- test/integration/sugar/table/active.test.js
+++ test/integration/sugar/table/active.test.js
@@ -11,33 +11,35 @@ describe("client.active(table)", function () {
   var client = require("../../../support/testClient");
 
   it("should throw an error if the maximum of attempts exceeded", function () {
-    active.__set__("read", function () {
-      return new Promise(function (resolve) {
-        resolve({TableStatus: "DELETING"});
+    active.__with__({
+      read: function () {
+        return new Promise(function (resolve) {
+            resolve({TableStatus: "DELETING"});
+          }
+        )}
+    })(function () {
+      return active('', '')
+    })
+      .catch(function (err) {
+        expect(err).to.be.instanceof(Error);
+        expect(err.message).to.equal("Exceeded number of attempts");
       });
-    });
-
-    active.__set__("tries", 60);
-
-    return active('', '').then(function onSuccess() {}, function onError(err) {
-      expect(err).to.be.instanceof(Error);
-      expect(err.message).to.equal("Exceeded number of attempts");
-    });
   });
 
   it("should return 'ACTIVE' if anything is fine", function () {
-    active.__set__("read", function () {
-      return new Promise(function (resolve) {
-        resolve({TableStatus: "ACTIVE"});
+    active.__with__({
+      read: function () {
+        return new Promise(function (resolve) {
+            resolve({TableStatus: "ACTIVE"});
+          }
+        )}
+    })(function () {
+      return active('', '')
+    })
+      .then(function (res) {
+        expect(res).to.have.property("TableStatus");
+        expect(res.TableStatus).to.equal("ACTIVE");
       });
-    });
-
-    active.__set__("tries", 0)
-
-    return active('','').then(function onSuccess(res) {
-      expect(res).to.have.property("TableStatus");
-      expect(res.TableStatus).to.equal("ACTIVE");
-    });
   });
 
 });
